da8c9efb5ae59c737266ddce38e3f44ebafdef9d
Erich <59769856+erichfi@users.noreply.github.com>
2023-01-21 15:44:54 +0000

    Update QV.py

--- QV.py
+++ QV.py
@@ -1,34 +1,27 @@
 import math
 
-def clusterMatch(groups, contributions):
+def cluster_match(groups, contributions):
     """
-    Finds the contribution of each group and the contribution of each pair of groups
-    to the result.
-
-    Parameters:
-    groups (list of lists of integers): bag of groups.
-    contributions (list of floats): array of contributions.
-
-    Returns:
-    float: the square root of the result.
+    groups: bag of groups (list of lists of integers)
+    contributions: list of contributions (floats)
+    group_memberships: list of sets (lists of lists) where
+                        group_memberships[i] is the groups agent i belongs to
     """
-    group_memberships = [[] for _ in contributions]
+    group_memberships = [[] for _ in range(len(contributions))]
 
-    # Build group memberships for each agent
-    for i, group in enumerate(groups):
-        for j in group:
+    for i in range(len(groups)):
+        for j in groups[i]:
             group_memberships[j].append(i)
 
     def common_group(i, j):
         """
-        Returns True if voter i and voter j share any common group. Else returns False.
+        If voter i and voter j share any common group, return true. Else return false.
         """
         return any(group in group_memberships[j] for group in group_memberships[i])
 
     def K(i, group):
         """
-        Returns the square root of contributions[i] if group includes voter i, or any member
-        of group shares a different group with voter i. Else returns contributions[i].
+        If group includes voter i, or any member of group shares a different group with voter i
         """
         if i in group or any(common_group(i, j) for j in group):
             return math.sqrt(contributions[i])
@@ -36,12 +29,10 @@ def clusterMatch(groups, contributions):
 
     result = 0
 
-    # Add the contribution of each agent in each group to the result
     for g in groups:
         for i in g:
             result += contributions[i] / len(group_memberships[i])
 
-    # Iterate over each pair of groups and add their contribution to the result
     for g in groups:
         for h in groups:
             if g == h:
